ee2d99687c99d3313ac8c36a76254645a8925b75
Rituja <rituja.dixit@xebia.com>
2020-06-01 15:29:42 +0530

    price and discount display added

--- src/views/filterBarView.jsx
+++ src/views/filterBarView.jsx
@@ -43,6 +43,77 @@ const filterBarView = ( payload ) => {
                         })
                     }
                 </ul>
+                <div className='filter-wrap'>PRICE</div>
+                <div className="slide-filter">
+                    <div className="control-box">
+                        <input className="checkbox" type="checkbox" id="chk_Rs. 0 - Rs. 499_0-499" value="on" />
+                        <label className="control--checkbox" for="chk_Rs. 0 - Rs. 499_0-499">
+                            <span>Rs. 0 - Rs. 499</span>
+                            {/* <div className="control__indicator"></div> */}
+                        </label>
+                    </div>
+                    <div className="control-box">
+                        <input className="checkbox" type="checkbox" id="chk_Rs. 0 - Rs. 499_0-499" value="on" />
+                        <label className="control--checkbox" for="chk_Rs. 0 - Rs. 499_0-499">
+                            <span>Rs. 0 - Rs. 499</span>
+                            {/* <div class="control__indicator"></div> */}
+                        </label>
+                    </div>
+                    <div className="control-box">
+                        <input className="checkbox" type="checkbox" id="chk_Rs. 0 - Rs. 499_0-499" value="on" />
+                        <label className="control--checkbox" for="chk_Rs. 0 - Rs. 499_0-499">
+                            <span>Rs. 500 - Rs. 999</span>
+                            {/* <div class="control__indicator"></div> */}
+                        </label>
+                    </div>
+                    <div className="control-box">
+                        <input className="checkbox" type="checkbox" id="chk_Rs. 0 - Rs. 499_0-499" value="on" />
+                        <label className="control--checkbox" for="chk_Rs. 0 - Rs. 499_0-499">
+                            <span>Rs. 1000 - Rs. 1999</span>
+                            {/* <div class="control__indicator"></div> */}
+                        </label>
+                    </div>
+                    <div className="control-box">
+                        <input className="checkbox" type="checkbox" id="chk_Rs. 0 - Rs. 499_0-499" value="on" />
+                        <label className="control--checkbox" for="chk_Rs. 0 - Rs. 499_0-499">
+                            <span>Rs. 4000 & Above</span>
+                            {/* <div class="control__indicator"></div> */}
+                        </label>
+                    </div>
+                </div>
+                <div className='filter-wrap'>DISCOUNT</div>
+                <div className="slide-filter">
+                    <div className="control-box">
+                        <input className='checkbox' type="radio" id="rdo_40% and above_40-*" name="40% and above" value="on" />
+                        <label className="control--checkbox" for="chk_Rs. 0 - Rs. 499_0-499">
+                            <span>40% and above</span>
+                        </label>
+                    </div>
+                    <div className="control-box">
+                        <input className='checkbox' type="radio" id="rdo_40% and above_40-*" name="40% and above" value="on" />
+                        <label className="control--checkbox" for="chk_Rs. 0 - Rs. 499_0-499">
+                            <span>30% and above</span>
+                        </label>
+                    </div>
+                    <div className="control-box">
+                        <input className='checkbox' type="radio" id="rdo_40% and above_40-*" name="40% and above" value="on" />
+                        <label className="control--checkbox" for="chk_Rs. 0 - Rs. 499_0-499">
+                            <span>20% and above</span>
+                        </label>
+                    </div>
+                    <div className="control-box">
+                        <input className='checkbox' type="radio" id="rdo_40% and above_40-*" name="40% and above" value="on" />
+                        <label className="control--checkbox" for="chk_Rs. 0 - Rs. 499_0-499">
+                            <span>10% and above</span>
+                        </label>
+                    </div>
+                    <div className="control-box">
+                        <input className='checkbox' type="radio" id="rdo_40% and above_40-*" name="40% and above" value="on" />
+                        <label className="control--checkbox" for="chk_Rs. 0 - Rs. 499_0-499">
+                            <span>Upto 10%</span>
+                        </label>
+                    </div>
+                </div>
             </div>
         </>
     )
@@ -53,7 +124,6 @@ const filterBarView = ( payload ) => {
                 <h3>Filters</h3>
                 { isLoading ? <div>Loading...</div> : view }
             </div>
-            
         </div>
     )
 
